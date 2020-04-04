721f99aed1457fdbb99b62b46b2a346963113876
melihbaltacii <melih.baltaci.60@gmail.com>
2020-04-04 19:30:20 +0300

    setting update

--- covid19/settings/developmentsettings.py
+++ covid19/settings/developmentsettings.py
@@ -15,7 +15,7 @@ DATABASES = {
 }
 
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 # STATICFILES_DIRS = (
-#     os.path.join(BASE_DIR, 'staticfiles'),
+#     os.path.join(BASE_DIR, 'static'),
 # )
